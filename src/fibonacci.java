5651794246425761eb3c84fcd7a9f5999b38b2ad
KushalSrivatsa2001 <52009913+KushalSrivatsa2001@users.noreply.github.com>
2020-09-18 10:45:50 +0530

    FIBONACCI

--- src/fibonacci.java
+++ src/fibonacci.java
@@ -1,2 +1,27 @@
-public class fibonacci {
+import java.util.Scanner;
+public class fibonacci
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number of terms");
+        int n = sc.nextInt();
+        int first = 0, second = 1, next;
+        System.out.println("Fibonacci series:");
+        int i = 0;
+        while(i<n)
+        {
+            if(i<=1)
+                next = i;
+            else
+            {
+                next=first+second;
+                first=second;
+                second=next;
+            }
+            System.out.println(next);
+            i++;
+        }
+    }
 }
+
